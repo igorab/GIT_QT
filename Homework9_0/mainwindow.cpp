3a534308bd4871957a582d22f60cb108f25d9664
Igor <igor.balakirev@mail.ru>
2021-10-13 17:22:20 +0300

    planet rotation 13102021

--- Homework9_0/mainwindow.cpp
+++ Homework9_0/mainwindow.cpp
@@ -4,15 +4,68 @@
 #include "QImage"
 #include "QPixmap"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainWindow)
 {
+    speed_planet_rotation = 2.5;
+
+    planet_size = 500;
+
     ui->setupUi(this);
+
+    star_background.load("img/star_background.jpg");
+
+    planet_texture.load("img/planet_texture.png");
+
+    planet_alpha.load("img/planet_alpha.png");
+
+    planet_alpha = planet_alpha.scaled(planet_size, planet_size, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+    timer_cycle = new QTimer();
+
+    connect(timer_cycle, SIGNAL(timeout()), this, SLOT(cycle()));
+
+    timer_cycle->start(20);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+
+    painter.setViewport(ui->screen->geometry().x(), ui->screen->geometry().y(), ui->screen->geometry().width(), ui->screen->geometry().height());
+
+    star_background = star_background.scaled(ui->screen->geometry().width(), ui->screen->geometry().height(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+    painter.drawImage(0, 0, star_background);
+    painter.save();
+
+    painter.translate(ui->screen->geometry().width()/2, ui->screen->geometry().height()/2);
+
+    painter.drawImage(-planet_size/2, -planet_size/2, planet);
+
+}
+
+void MainWindow::cycle()
+{
+    planet = planet_texture.copy(planet_rotation, 0, planet_texture.height(), planet_texture.height());
+
+    planet = planet.scaled(planet_size, planet_size, Qt::IgnoreAspectRatio, Qt::SmoothTransformation );
+
+
+    QPainter painter(&planet);
+    painter.setCompositionMode(QPainter::CompositionMode_DestinationIn);
+    painter.drawImage(0, 0, planet_alpha );
+
+
+    planet_rotation += speed_planet_rotation;
+
+    if (planet_rotation + planet_texture.height() >= planet_texture.width())
+        planet_rotation = 0;
+
+    update();
 }
 
--- Homework9_0/mainwindow.h
+++ Homework9_0/mainwindow.h
@@ -3,6 +3,12 @@
 
 #include <QMainWindow>
 
+#include <QPainter>
+#include <QTimer>
+#include <iostream>
+
+using namespace std;
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -12,10 +18,57 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
+    QTimer *timer_cycle;
+
+    int planet_size;
+
+    int atmosphere_up_size;
+
+    double speed_planet_rotation;
+
+    double planet_rotation = 0;
+
+    double speed_atmosphere_rotation;
+
+    QImage star_background;
+
+    QImage planet_texture;
+
+    QImage planet_alpha;
+
+    QImage planet;
+
+    QImage planet_shadow;
+
+    QImage planet_lights_texture;
+
+    QImage planet_lights_alpha;
+
+    QImage planet_lights;
+
+    QImage planet_atmosphere_texture;
+
+    QImage planet_atmosphere_alpha;
+
+    QImage planet_atmosphere;
+
+
+
+
 private:
+
     Ui::MainWindow *ui;
+
+private slots:
+
+    void paintEvent(QPaintEvent *event);
+    void cycle();
+
 };
+
 #endif // MAINWINDOW_H
