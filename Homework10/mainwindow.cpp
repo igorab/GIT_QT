7f455c0dd75597ef35e6dd28053034965d5d2626
Igor <igor.balakirev@mail.ru>
2021-10-28 18:33:48 +0300

    planet 28102021

--- Homework10/mainwindow.cpp
+++ Homework10/mainwindow.cpp
@@ -6,6 +6,61 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->centralwidget->installEventFilter(this);
+
+    ui->centralwidget->setMouseTracking(true);
+
+    x_axis = 0;
+    y_axis = 0;
+    scale_scene = 1;
+    orbital_size = 280;
+    orbital_speed = 0.2;
+    speed_planet_rotation = 2.5;
+    speed_atmosphere_rotation = 2.9;
+    planet_size = 150;
+    atmosphere_up_size = 6;
+    moon_size=50;
+    orbital_moon_size=120;//размерорбитыспутника
+    orbital_moon_speed=1.6;//орбитальнаяскоростьспутника
+    star_size=200;//размерзвезды
+    speed_moon_rotation=2.5;//скоростьвращенияспутника
+    speed_star_rotation=2.5;//скоростьвращениязвезды
+
+    //загрузказвездногофона
+    star_background.load("img/star_background.jpg");
+    //загрузкатекстурнойкартыпланеты
+    planet_texture.load("img/planet_texture.png");
+    //загрузкамаскиальфа-смешиванияпланеты
+    planet_alpha.load("img/planet_alpha.png");
+    //масштабируем маску альфа-смешивания планеты под заданный размер планеты
+    planet_alpha=planet_alpha.scaled(planet_size,planet_size,Qt::IgnoreAspectRatio,Qt::SmoothTransformation);
+    //загрузка тени планеты
+    planet_shadow.load("img/planet_shadow.png");
+    //масштабируем тень планеты подзаданный размер планеты
+    planet_shadow=planet_shadow.scaled(planet_size,planet_size,Qt::IgnoreAspectRatio,Qt::SmoothTransformation);
+    //загрузка текстурной карты ночных огней городов
+    planet_lights_texture.load("img/planet_lights_texture.png");
+
+    //масштабируемтекстурнуюкартуночныхогнейгородовподразмертекстурнойкартыпланеты
+    planet_lights_texture = planet_lights_texture.scaled(planet_texture.width(), planet_texture.height(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    //загрузкамаскиальфа-смешиванияночныхогнейгородов
+    planet_lights_alpha.load("img/planet_lights_alpha.png");
+    //масштабируеммаскуальфа-смешиванияночныхогнейгородовподзаданныйразмерпланеты
+    planet_lights_alpha=planet_lights_alpha.scaled(planet_size, planet_size, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+    //загрузкатекстурнойкартыатмосферы
+    planet_atmosphere_texture.load("img/planet_atmosphere_texture.png");
+
+    //масштабируемтекстурнуюкартуатмосферыподзаданныйразмертекстурнойкартыпланеты
+    planet_atmosphere_texture = planet_atmosphere_texture.scaled(planet_texture.width(), planet_texture.height(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+    //загрузкамаскиальфа-смешиванияатмосферы
+    planet_atmosphere_alpha.load("img/planet_atmosphere_alpha.png");
+
+    //масштабируеммаскуальфа-смешиванияатмосферыподзаданныйразмерпланеты+увеличенныйразмершапкиатмосферы
+    planet_atmosphere_alpha = planet_atmosphere_alpha.scaled(planet_size + atmosphere_up_size*2, planet_size + atmosphere_up_size*2, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
 }
 
 MainWindow::~MainWindow()
--- Homework10/mainwindow.h
+++ Homework10/mainwindow.h
@@ -2,8 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPainter>
+#include <QTimer>
+
 #include <QKeyEvent>
 #include <QMouseEvent>
+#include <QtMath>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,18 +20,115 @@ class MainWindow : public QMainWindow
 public:
 
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
 
-    void keyPressEvent(QKeyEvent *event);
+    QTimer *timer_cycle;
+
+    bool show_info = false;
+
+    QString text_info;
+
+    int coord_info_x;
+    int coord_info_y;
+
+    double scale_scene;
+
+    double x_axis = 1; //вращение мира по оси X
+    double y_axis = 1; //вращение мира по оси Y
+
+    //размерспутникапланеты
+    int moon_size = 50;
+    //размерорбитыспутника
+    int orbital_moon_size = 220;
+    //орбитальнаяскоростьспутника
+    double orbital_moon_speed = 2.6;
+    //размер звезды
+    int star_size = 200;
+    //скорость вращения спутника
+    double speed_moon_rotation = 2.5;
+    // скорость вращения звезды
+    double speed_star_rotation = 2.5;
+    //счетчик вращения орбиты спутника
+    double orbital_moon_rotation = 0;
+
+    //счетчиквращенияспутника
+    double moon_rotation;
+    //счетчиквращениязвезды
+    double star_rotation;
+    //размерорбиты
+    int orbital_size;
+    //орбитальнаяскорость
+    double orbital_speed;
+    //счетчиквращенияорбиты
+    double orbital_rotation=0;
+    //размерпланеты
+    int planet_size;
+    //насколькоувеличиваетсяшапкаатмосферы
+    int atmosphere_up_size;
+    //скоростьвращенияпланеты
+    double speed_planet_rotation;
+    //счетчиквращенияпланеты
+    double planet_rotation=0;
+    //скоростьвращенияатмосферы
+    double speed_atmosphere_rotation;
+    //счетчиквращенияатмосферы
+    double atmosphere_rotation=0;
+
+    //звездный фон
+    QImage star_background;
+    //текстурная карта планеты
+    QImage planet_texture;
+    //маска альфа-смешивания планеты
+    QImage planet_alpha;
+    //итоговый объект планеты
+    QImage planet;
+    //тень планеты
+    QImage planet_shadow;
+    //текстурная карта спутника планеты
+    QImage moon_texture;
+    //маска альфа-смешивания спутника
+    QImage moon_alpha;
+    //итоговый объект спутника
+    QImage moon;
+    //тень спутника
+    QImage moon_shadow;
+    //текстурная карта звезды
+    QImage star_texture;
+    //маска альфа-смешивания звезды
+    QImage star_alpha;
+    //итоговый объект звезды
+    QImage star;
+    //текстурная карта ночных огней городов
+    QImage planet_lights_texture;
+    //маска альфа-смешивания ночных огней городов
+    QImage planet_lights_alpha;
+    //итоговый объект ночных огней городов
+    QImage planet_lights;
+    //текстурная карта атмосферы
+    QImage planet_atmosphere_texture;
+    //маска альфа-смешивания атмосферы
+    QImage planet_atmosphere_alpha;
+    //итоговый объект атмосферы
+    QImage planet_atmosphere;
+
+    ~MainWindow();
 
 protected:
 
     void mousePressEvent(QMouseEvent *event);
 
-    bool eventFilter(QObject*target, QEvent*event);
+    void keyPressEvent(QKeyEvent *event);
+
+    bool eventFilter(QObject *target, QEvent *event);
 
 private:
     Ui::MainWindow *ui;
 
+private slots:
+
+    void paintEvent(QPaintEvent *event);
+
+    void cycle();
+
 };
+
 #endif // MAINWINDOW_H
