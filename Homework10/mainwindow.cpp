56936838f164dd26779cb99b03545bf17883f42b
Igor <igor.balakirev@mail.ru>
2021-11-01 18:51:39 +0300

    01112021 cycle

--- Homework10/mainwindow.cpp
+++ Homework10/mainwindow.cpp
@@ -1,14 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
+    //необходимо назначить фильтр для центрального виджета, в котором реализуем обработчик событий мыши
+
+    // становим фильтр событий на центральный виджет
     ui->centralwidget->installEventFilter(this);
 
+    // становим отслеживание движений мыши
     ui->centralwidget->setMouseTracking(true);
 
     x_axis = 0;
@@ -49,19 +51,171 @@ MainWindow::MainWindow(QWidget *parent)
     //масштабируеммаскуальфа-смешиванияночныхогнейгородовподзаданныйразмерпланеты
     planet_lights_alpha=planet_lights_alpha.scaled(planet_size, planet_size, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
 
-    //загрузкатекстурнойкартыатмосферы
+    //загрузка текстурной карты атмосферы
     planet_atmosphere_texture.load("img/planet_atmosphere_texture.png");
 
-    //масштабируемтекстурнуюкартуатмосферыподзаданныйразмертекстурнойкартыпланеты
+    //масштабируем текстурную карту атмосферы под заданный размер текстурной карты планеты
     planet_atmosphere_texture = planet_atmosphere_texture.scaled(planet_texture.width(), planet_texture.height(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
 
     //загрузкамаскиальфа-смешиванияатмосферы
     planet_atmosphere_alpha.load("img/planet_atmosphere_alpha.png");
 
-    //масштабируеммаскуальфа-смешиванияатмосферыподзаданныйразмерпланеты+увеличенныйразмершапкиатмосферы
+    //масштабируем маску альфа-смешивания атмосферы под заданный размер планеты + увеличенный размер шапки атмосферы
     planet_atmosphere_alpha = planet_atmosphere_alpha.scaled(planet_size + atmosphere_up_size*2, planet_size + atmosphere_up_size*2, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
 
+    /*
+     * загружаем текстуры новых объектов (Луны и Солнца) аналогичным образом, при этом альфа-маску-круга и альфа-маску-тени используем существующие, от планеты
+     */
+
+    //загрузкатекстурнойкартыспутникапланеты
+    moon_texture.load("img/moon.png");
+
+    //загрузкамаскиальфа-смешиванияспутника
+    moon_alpha.load("img/planet_alpha.png");
+
+    //масштабируем маску альфа-смешивания спутника планеты под заданный размер
+    moon_alpha = moon_alpha.scaled(moon_size,moon_size,Qt::IgnoreAspectRatio,Qt::SmoothTransformation);
+
+    //масштабируем тень спутника планеты под заданный размер планеты
+    moon_shadow = planet_shadow.scaled(moon_size,moon_size,Qt::IgnoreAspectRatio,Qt::SmoothTransformation);
+
+    //загрузка текстурной карты звезды
+    star_texture.load("img/sun.png");
+
+    //загрузка маски альфа-смешивания звезды
+    star_alpha.load("img/planet_alpha.png");
+
+    //масштабируеммаскуальфа-смешиваниязвездыподзаданныйразмер
+    star_alpha = star_alpha.scaled(star_size,star_size,Qt::IgnoreAspectRatio,Qt::SmoothTransformation);
+
+    //инициализацияобъектатаймера
+    timer_cycle = new QTimer();
+
+    //соединим сигнал таймера со слотом обновления экрана
+    connect(timer_cycle,SIGNAL(timeout()),this,SLOT(cycle()));
+
+    //запустим таймер
+    timer_cycle -> start(20);
+
 }
+
+void MainWindow::cycle()
+{
+    //копированиеучасткатекстурывсоответствииспозициейсчетчикавращенияпланеты
+    planet=planet_texture.copy(planet_rotation,0,planet_texture.height(),planet_texture.height());
+
+    //масштабируемитоговоеизображениепланетыдозаданногоразмера
+    planet=planet.scaled(planet_size,planet_size,Qt::IgnoreAspectRatio,Qt::SmoothTransformation);
+
+    //копированиеучасткатекстурыогнейгородоввсоответствииспозициейсчетчикавращенияпланеты
+    planet_lights=planet_lights_texture.copy(planet_rotation,0,planet_lights_texture.height(),planet_lights_texture.height());
+
+    //масштабируемитоговоеизображениеогнейгородовдозаданногоразмерапланеты
+    planet_lights=planet_lights.scaled(planet_size,planet_size,Qt::IgnoreAspectRatio,Qt::SmoothTransformation);
+
+    //копированиеучасткатекстурыатмосферывсоответствииспозициейсчетчикавращенияатмосферы
+    planet_atmosphere = planet_atmosphere_texture.copy(atmosphere_rotation,0,planet_atmosphere_texture.height(),planet_atmosphere_texture.height());
+
+    //масштабируем итоговое изображение атмосферы до заданного размерапланеты + увеличение размера на размер шапки атмосферы
+    planet_atmosphere = planet_atmosphere.scaled(planet_size+atmosphere_up_size*2, planet_size+atmosphere_up_size*2, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+    //копированиеучасткатекстурывсоответствииспозициейсчетчикавращенияспутника
+    //Примечание: подготовим новые объекты, аналогично планете.
+    moon = moon_texture.copy(moon_rotation,0,moon_texture.height(),moon_texture.height());
+    //масштабируемитоговоеизображениеспутникадозаданногоразмера
+    moon = moon.scaled(moon_size,moon_size,Qt::IgnoreAspectRatio,Qt::SmoothTransformation);
+    //копированиеучасткатекстурывсоответствииспозициейсчетчикавращениязвезды
+    star = star_texture.copy(star_rotation,0,star_texture.height(),star_texture.height());
+    //масштабируемитоговоеизображениезвездыдозаданногоразмера
+    star = star.scaled(star_size,star_size,Qt::IgnoreAspectRatio,Qt::SmoothTransformation);
+
+    //инициализируем рисовальщик для преобразования итогового изображения планеты
+    QPainter painter(&planet);
+
+    //установим режимкомпозицииальфа-смешивания-прозрачныйцветальфа-маскископируетсянаитоговоеизображение
+    painter.setCompositionMode(QPainter::CompositionMode_DestinationIn);
+
+    //отрисовкавобъектитоговогоизображенияпланетыальфамаски
+    painter.drawImage(0,0,planet_alpha);
+
+    //инициализируем рисовальщик для преобразования итогового изображения звезды
+    QPainter ps(&star);
+
+    //установим режим композицииальфа-смешивания-прозрачныйцветальфа-маскископируетсянаитоговоеизображение
+    ps.setCompositionMode(QPainter::CompositionMode_DestinationIn);
+
+    //отрисовка в объект итогового изображения звезды альфа маски
+    ps.drawImage(0,0,star_alpha);
+
+    //инициализируем рисовальщик для преобразования итогового изображения спутника
+    QPainter pm(&moon);
+
+    //установим режим композиции альфа-смешивания-прозрачный цвет альфа-маскис копируется на итоговое изображение
+    pm.setCompositionMode(QPainter::CompositionMode_DestinationIn);
+
+    //отрисовка в объект итогового изображения спутника альфа маски
+    pm.drawImage(0,0,moon_alpha);
+
+    //инициализируем рисовальщик для преобразования итогового изображения ночных огней планеты
+    QPainter painter2(&planet_lights);
+
+    //установим режим композиции альфа-смешивания-прозрачный цвет альфа-маски скопируется на итоговое изображение
+    painter2.setCompositionMode(QPainter::CompositionMode_DestinationIn);
+
+    //отрисовка в объектизображения планеты альфа маски для отсечения углов
+    painter2.drawImage(0,0,planet_alpha);
+
+    //отцентрируемсцену
+    painter2.translate(planet_size/2,planet_size/2);
+    //повернемсценудляповоротаальфамаскисогласноосвещению
+
+    painter2.rotate(orbital_rotation);
+    //отрисовкавобъектитоговогоизображенияогнейпланетыальфамаски
+    painter2.drawImage(-planet_size/2,-planet_size/2,planet_lights_alpha);
+
+    //инициализируем рисовальщик для преобразования итогового изображения атмосферы планеты
+    QPainter painter3(&planet_atmosphere);
+
+    //установим режим композиции альфа-смешивания-прозрачный цвет альфа-маски скопируется на итоговое изображение
+    painter3.setCompositionMode(QPainter::CompositionMode_DestinationIn);
+    //отрисовкавобъектитоговогоизображенияатмосферыпланетыальфамаски
+    painter3.drawImage(0,0,planet_atmosphere_alpha);
+    //увеличимсчетчиквращенияорбиты
+    orbital_rotation+=orbital_speed;
+    //обнулимсчетчикорбиты,еслипрошлиполныйкруг
+    if(orbital_rotation>360) orbital_rotation=0;
+    //увеличимсчетчиквращенияпланетынавеличинускорости
+    planet_rotation+=speed_planet_rotation;
+    //если значение счетчика выходит за рамки размера текстуры, то обнулить счетчик
+    if (planet_rotation+planet_texture.height()>=planet_texture.width()) planet_rotation=0;
+    //увеличим счетчик вращения атмосферы планеты на величину скорости
+    atmosphere_rotation += speed_atmosphere_rotation;
+    //если значение счетчика выходит за рамки размера текстуры, то обнулить счетчик
+    if (atmosphere_rotation + planet_atmosphere_texture.height() >= planet_atmosphere_texture.width()) atmosphere_rotation =0;
+
+    // Примечание: аналогично установим счетчики вращения Солнца и Луны,а также счетчик вращения орбиты Луны.
+
+    // увеличим счетчик вращения спутника на величину скорости
+    moon_rotation += speed_moon_rotation;
+
+    //если значение счетчика выходитзарамкиразмератекстуры,тообнулитьсчетчик
+    if (moon_rotation+moon_texture.height() >= moon_texture.width()) moon_rotation = 0;
+
+    // увеличим счетчик вращения планеты на величину скорости
+    star_rotation += speed_star_rotation;
+
+    //если значение счетчика выходит за рамки размера текстуры, то обнулить счетчик
+    if (star_rotation + star_texture.height()>=star_texture.width()) star_rotation = 0;
+
+    // увеличим счетчик вращения орбиты спутника
+    orbital_moon_rotation += orbital_moon_speed;
+    //обнулим счетчик орбитыспутника,еслипрошлиполныйкруг
+    if (orbital_moon_rotation > 360) orbital_moon_rotation = 0;
+
+    //обновим экран
+    update();
+}
+
 
 MainWindow::~MainWindow()
 {
