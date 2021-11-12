ce4534cbf7414ea76a3fb98683856f7f5c3e6cad
Igor <igor.balakirev@mail.ru>
2021-11-12 17:20:45 +0300

    lng 12112021

--- HW6_Calculator/calculator.cpp
+++ HW6_Calculator/calculator.cpp
@@ -1,8 +1,72 @@
 #include "calculator.h"
 
+//Стандарт языка C++17
+//Пример ввода: 8 9 + 1 7 - *
+
+//______Реализация______
+
+#include <iostream>
+#include <string>
+#include <functional>
+#include <vector>
+#include <map>
+#include <cctype>
+
+using namespace std;
+
+int pexpression()
+{
+    map<char, function<int64_t(const int64_t&, const int64_t&)>> operations;
+    operations['+'] = [](const int64_t& a, const int64_t& b) constexpr { return a + b; };
+    operations['-'] = [](const int64_t& a, const int64_t& b) constexpr { return a - b; };
+    operations['*'] = [](const int64_t& a, const int64_t& b) constexpr { return a * b; };
+    operations['/'] = [](const int64_t& a, const int64_t& b) constexpr { return a / b; };
+
+    string expression;
+    vector<int64_t> stack_;
+    int64_t number = 0;
+    bool flag = true;
+
+    getline(cin, expression);
+
+    for (const auto& i : expression)
+    {
+        if (isdigit(i))
+        {
+            number *= 10;
+            number += (i - '0');
+            flag = true;
+        }
+        else
+        {
+            if (i != ' ')
+            {
+                int64_t num2 = stack_.back();
+                stack_.pop_back();
+                int64_t num1 = stack_.back();
+                stack_.pop_back();
+
+                stack_.push_back(operations[i](num1, num2));
+                flag = false;
+            }
+            else if (i == ' ' && flag)
+            {
+                stack_.push_back(number);
+                number = 0;
+            }
+        }
+    }
+
+    cout << stack_.back();
+
+    return 0;
+}
+
+
+
 Calculator::Calculator()
 {
-
+    pexpression();
 
 }
 
